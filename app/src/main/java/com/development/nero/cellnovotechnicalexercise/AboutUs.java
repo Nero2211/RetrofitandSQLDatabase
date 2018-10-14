96d97e273d5754c326ea7031091345caf1edf20d
Nero2211 <kartik_verma@hotmail.com>
2018-10-14 01:08:37 +0100

    12 commit 1. About us page has been implemented with collpasing toolbar and card view with information obtained from CallNovo website. 2. Will need to provide final touches and generate a logo media for the splash screen

--- app/src/main/java/com/development/nero/cellnovotechnicalexercise/AboutUs.java
+++ app/src/main/java/com/development/nero/cellnovotechnicalexercise/AboutUs.java
@@ -1,13 +1,76 @@
 package com.development.nero.cellnovotechnicalexercise;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class AboutUs extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RelativeLayout youtube, linkedin, facebook;
+    Button visitCallNovo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        toolbar = findViewById(R.id.toolbar);
+        youtube = findViewById(R.id.youtubeView);
+        linkedin = findViewById(R.id.linkedinView);
+        facebook = findViewById(R.id.facebookView);
+        visitCallNovo = findViewById(R.id.visitCellNovoBtn);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onBackPressed();
+                }
+            });
+        }
+
+        visitCallNovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openURL("https://www.cellnovo.com/en/homepage");
+            }
+        });
+
+        youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openURL("https://www.youtube.com/channel/UCtWIF_oP82-jCO_afd9mwCw");
+            }
+        });
+
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openURL("https://www.linkedin.com/company/cellnovo-ltd/");
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openURL("https://www.facebook.com/Cellnovo-170324893037062/");
+            }
+        });
+    }
+
+    public void openURL(String url){
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/development/nero/cellnovotechnicalexercise/SplashActivity.java
+++ app/src/main/java/com/development/nero/cellnovotechnicalexercise/SplashActivity.java
@@ -47,6 +47,7 @@ public class SplashActivity extends AwesomeSplash {
             public void run() {
                 Intent intent = new Intent(SplashActivity.this, HomeScreen.class);
                 startActivity(intent);
+                finish();
             }
         }, 2000);
     }
